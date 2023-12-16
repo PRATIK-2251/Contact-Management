436e8e8806d813de36a9c5f4ce6ae41e19140e8d
PRATIK-2251 <pratikhole22@gmail.com>
2023-12-16 20:20:23 +0530

    Revert "create route in contact router list- get,post,put,delete methods"
    This reverts commit 4c3e4e99d4d0b23060611de9b606ff2044ee76d9.

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -1,32 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
-// GET all contact
 router.route("/").get((request, response) => {
-  response.status(200).send({ message: "Gel all contacts" });
-});
-
-// GET single contact
-router.route("/:id").get((request, response) => {
-  response
-    .status(200)
-    .send({ message: `Single contact is get ${request.params.id}` });
-});
-
-router.route("/").post((request, response) => {
-  response.status(201).send({ message: "Created contact" });
-});
-
-router.route("/:id").put((request, response) => {
-  response
-    .status(200)
-    .send({ message: `Update contact of id is  ${request.params.id}` });
-});
-
-router.route("/:id").delete((request, response) => {
-  response
-    .status(200)
-    .send({ message: `Deleted contact is ${request.params.id}` });
+  response.status(200).send({ message: "Welcome to API" });
 });
 
 module.exports = router;
